188a4fbba68fb1b3e1f0ad23949725e1cc2e8675
Chandan Gowda K S <chanduchess122@gmail.com>
2026-01-21 20:20:06 +0530

    Initial commit

--- Mongo_express/index.js
+++ Mongo_express/index.js
@@ -3,11 +3,13 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Chat = require("./modules/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
@@ -27,7 +29,7 @@ app.get("/chats/new", (req, res) => {
 //index route
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    console.log(chats);
+    // console.log(chats);
     res.render("./whatsapp", { chats });
 });
 // create route
@@ -48,6 +50,34 @@ app.post("/chats", (req, res) => {
     });
     res.redirect("/chats");
 })
+
+// edit route
+app.get("/chats/:id/edit", async(req, res) => {
+    let { id } = req.params;
+    let chatId = await Chat.findById(id);
+    res.render("edit.ejs", { chatId });
+})
+
+// update route
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(
+        id,
+        { msg: newMsg },
+        { runValidators: true, new: true }
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+// destroy route // delete chat
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let deleted_chart = await Chat.findByIdAndDelete(id);
+    console.log(deleted_chart);
+    res.redirect("/chats");
+});
 app.listen(3000, () => {
     console.log("port is listenning");
 });
