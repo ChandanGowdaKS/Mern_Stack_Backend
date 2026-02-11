8a451ed3a982c52ef4ff3cb756ab388e872f8fae
Chandan Gowda K S <chanduchess122@gmail.com>
2026-02-11 22:20:53 +0530

    Initial commit

--- Middle-wares/app.js
+++ Middle-wares/app.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    let { query } = req.query;
-    console.log(query);
-    res.send("Middleware completed");
+// Simple middleware
+// app.use((req, res,next) => {
+//     let { query } = req.query;
+//     console.log(query);
+//     console.log("This is 1st Middleware");
+    
+//     next();// calls next middleware or next routes
+// });
+
+// app.use((req, res, next) => {
+//     console.log("This is 2nd Middleware");
+//     next();
+// });
+
+// API Token as Query String
+const checkToken = (req, res, next) => {
+    let { token } = req.query;
+    if (token === "giveaccess") {
+        next();
+    }
+    throw new Error("ACCESS DENIED!");
+};
+
+app.use("/api", checkToken,(req, res) => {
+    res.send("This is main API Route");
 });
+
+
+// Utility middleware logger
+
+app.use((req, res, next) => {
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
+    console.log(req.path);
+    next();
+});
+
 
 app.get("/", (req, res) => {
     res.send("root page");
