78f17dcca745b68aaa252e8d95604a4541894599
Chandan Gowda K S <chanduchess122@gmail.com>
2026-02-28 23:07:51 +0530

    Initial Commit

---
+++ DB relationship/Models/customer.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+main()
+    .then(() => console.log("Connection Successful"))
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/RelationshipInDB');
+}
+
+const orderSchema = new Schema({
+    item: String,
+    price : Number,
+});
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "order",
+        },
+    ],
+});
+
+const Customer = mongoose.model("Customer",customerSchema)
+const Order = mongoose.model("Order", orderSchema);
+
+
+
+
+// const addOrders = async () => {
+//     let res = await Order.insertMany([
+//         { item: "Samosa", price: 10 },
+//         { item: "chocolate", price: 5 },
+//         { item: "Chips", price: 30 }
+//     ]);
+//     console.log(res);
+// };
+
+// addOrders();
